d2a7dd34dd74961a57a3acad049357d9861112e6
katetcherry <56260842+katetcherry@users.noreply.github.com>
2019-10-28 16:06:31 +0200

    formatting settings were changed 2

--- sandbox/src/main/java/ua/qa/java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/qa/java/sandbox/MyFirstProgram.java
@@ -1,8 +1,7 @@
 package ua.qa.java.sandbox;
 
-public class MyFirstProgram
-{
-  public static void main(String[] args) {
-    System.out.println("Hello world");
-  }
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
